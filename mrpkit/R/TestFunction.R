c6264ea89621dfa57e3c35a44b4c90331d73c89b
Lauren Kennedy <lauren@Laurens-MacBook-Pro.local>
2020-07-09 09:24:37 +1000

    Added a package sub folder, mrpkit
    - added test function code.
    Use roxygen2 to build doc
    Edit R files and run
    devtools::document()

---
+++ mrpkit/R/TestFunction.R
@@ -0,0 +1,11 @@
+#' Subtract together two numbers
+#'
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+subtract <- function(x, y) {
+  x - y
+}
