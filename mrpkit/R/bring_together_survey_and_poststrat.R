1869fa68629d8fd5aa02d9a79202fb6e3c2d36b0
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2020-08-10 05:38:11 -0400

    Add comments to the functions

--- mrpkit/R/bring_together_survey_and_poststrat.R
+++ mrpkit/R/bring_together_survey_and_poststrat.R
@@ -14,20 +14,26 @@
 #'
 #' @export
 
-bring_together_survey_and_poststrat <- function(survey_data, aligned_levels, survey_variable_of_interest) {
+# This first 'helper' function joins the survey data levels with the aligned levels
+# To allow this to work the other way, we'd need another one that works for the poststrat dataset or to modify this one
+get_aligned_levels_for_survey <- function(some_survey_data, survey_variable_of_interest, aligned_levels_type){
+  some_survey_data %>%
+    select(survey_variable_of_interest) %>%
+    left_join(y = aligned_levels %>% filter(variable == aligned_levels_type) %>% select(-variable), by = setNames("sample", survey_variable_of_interest)) %>%
+    select(-survey_variable_of_interest) %>%
+    rename(setNames("popn", survey_variable_of_interest))
+}
 
-  go_get_em <- function(some_survey_data, survey_variable_of_interest, aligned_levels_type){
-    some_survey_data %>%
-      select(survey_variable_of_interest) %>%
-      left_join(y = aligned_levels %>% filter(variable == aligned_levels_type) %>% select(-variable), by = setNames("sample", survey_variable_of_interest)) %>%
-      select(-survey_variable_of_interest) %>%
-      rename(setNames("popn", survey_variable_of_interest))
-  }
+# This is the function that we want the user to use. It applies that helper function variously to the different columns
+bring_together_survey_and_poststrat <- function(survey_data, aligned_levels, survey_variable_of_interest) {
 
   survey_data <- survey_data %>% select(survey_variable_of_interest)
 
-  mashed_together <- purrr::map2_dfc(names(survey_data), unique(aligned_levels$variable), ~ go_get_em(survey_data, .x, .y))
+  mashed_together <- purrr::map2_dfc(names(survey_data), unique(aligned_levels$variable), ~ get_aligned_levels_for_survey(survey_data, .x, .y))
 
   return(mashed_together)
 
 }
+
+
+
