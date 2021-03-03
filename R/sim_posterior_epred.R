ab0987e5e4e604d89b88ea889b8dbe48317d6dbb
jgabry <jgabry@gmail.com>
2021-03-03 09:38:47 -0700

    cleanup sim_posterior_epred

--- R/sim_posterior_epred.R
+++ R/sim_posterior_epred.R
@@ -1,39 +1,34 @@
-
-#' sim_posterior_epred
+#' Generate approximate samples of predicted probabilities from a binomial
+#' `glmerMod` object.
 #'
-#' @name sim_posterior_epred
 #' @export
-#' @description Helper function to create predictive samples from a glmer class model
-#' Takes in model class and newdata to predict for
-#' Return a matrix with rows that represent posterior samples and columns that represent
-#' data that is being predicted for
-#' Only for binomial family models.
-#' @param object Model of type glmer class and binomial family
-#' @param newdata Data to predict for
-#' @param nsamples Number of samples
+#' @param object A binomial model fit using `lme4::glmer()`.
+#' @param newdata The data to use.
+#' @param nsamples Number of samples to generate.
+#' @return A matrix with `nrow(newdata)` rows and `nsamples` columns.
 #' @examples
-
-
-
-sim_posterior_epred  <- function(object, newdata, nsamples = 4000){
-  if(!class(object)=="glmerMod"){
-    stop("Object must a model type of glmerMod")
-  }else{
-    if(!family(object)$family=="binomial"){
-      stop("Model must be a binomial model type")
-    }else{
-      #Make predictions using merTools (summary stats not needed as we use this as a convenience to get
-      #the predicted samples)
-      predict_nd <- merTools::predictInterval(merMod = object, newdata = newdata,
-                                                         level = 0.95, n.sims = nsamples,
-                                                         stat = "median", type="probability",
-                                                         include.resid.var = TRUE,
-                                                         returnSims = TRUE,
-                                                         .parallel=FALSE)
-      #Obtain predictive samples
-      predicted_results <- plogis(attributes(predict_nd)$sim.results)
-
-      return(t(predicted_results))
-    }
+#'
+sim_posterior_epred  <- function(object, newdata, nsamples = 4000) {
+  if (!requireNamespace("merTools", quietly = TRUE)) {
+    stop("Please install the merTools package.", call. = FALSE)
   }
+  if (!inherits(object, "glmerMod")) {
+    stop("Object must have class 'glmerMod'.", call. = FALSE)
+  }
+  if (family(object)$family != "binomial") {
+    stop("Model family must be binomial.", call. = FALSE)
+  }
+  predict_nd <-
+    merTools::predictInterval(
+      merMod = object,
+      newdata = newdata,
+      level = 0.95,
+      n.sims = nsamples,
+      stat = "median",
+      type = "probability",
+      include.resid.var = TRUE,
+      returnSims = TRUE,
+      .parallel = FALSE
+    )
+  plogis(attr(predict_nd, "sim.results"))
 }
