6245700e5b8df5f2b2750e1edc2d107fddc331db
Dewi-Amaliah <dama0007@student.monash.edu>
2021-03-16 09:08:31 +1100

    Add documentation to the example data.

---
+++ R/sim_data_approx_voters_popn.R
@@ -0,0 +1,21 @@
+#' A Simulated Data of NLP Voters Approximate Population (Modelled after ACS)
+#'
+#' This is a generated data shows the approximate population of the
+#' Neverland Labor Party (NLP) Survey to demonstrate the usage of the package.
+#'
+#' @source Created in NeverlandVoters.R
+#'
+#' @format A data frame with columns:
+#'
+#' \describe{
+#'  \item{age2}{Age group}
+#'  \item{gender}{Gender}
+#'  \item{vote_for}{The voted party in the 2016 election}
+#'  \item{y}{Preference to NLP}
+#'  \item{wt}{Raked and trimmed survey weight}
+#' }
+#' @examples
+#'
+#'  approx_voters_popn
+#'
+"approx_voters_popn"
---
+++ R/sim_data_nlp_survey.R
@@ -0,0 +1,22 @@
+#' A Simulated Data of NLP Voters Survey
+#'
+#' This is a generated data shows the survey result of the preference
+#' to Neverland Labor Party (NLP)
+#' along with several demographic variables to demonstrate the usage of the package.
+#'
+#' @source Created in NeverlandVoters.R
+#'
+#' @format A data frame with columns:
+#'
+#' \describe{
+#'  \item{age1}{Age group}
+#'  \item{gender}{Gender}
+#'  \item{vote_for}{The party that the respondent vote for in the 2016 election}
+#'  \item{y}{Preference to NLP}
+#'  \item{wt}{Raked and trimmed survey weight}
+#' }
+#' @examples
+#'
+#'  nlp_survey
+#'
+"nlp_survey"
