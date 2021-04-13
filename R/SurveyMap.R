a6f1bece16625aa113a7138b6affe00328daf6b1
jgabry <jgabry@gmail.com>
2021-04-13 17:14:16 -0600

    questions and responses via named lists
    closes #35

--- R/SurveyMap.R
+++ R/SurveyMap.R
@@ -13,20 +13,47 @@
 #' they should be listed in that order, either descending or ascending.
 #'
 #' @examples
+#' head(feline_survey)
 #' feline_prefs <- SurveyData$new(
-#'   feline_survey[,c("age1","gender","pet_own","y")],
-#'   questions = list("Please identify your age group","Please select your gender","Which pet do you own?", "Response"),
-#'   responses = list(levels(feline_survey$age1),levels(feline_survey$gender),levels(feline_survey$pet_own),c("no","yes")),
+#'   data = feline_survey,
+#'   questions = list(
+#'     age1 = "Please identify your age group",
+#'     gender = "Please select your gender",
+#'     pet_own = "Which pet do you own?",
+#'     y = "Response"
+#'   ),
+#'   responses = list(
+#'     age1 = levels(feline_survey$age1),
+#'     gender = levels(feline_survey$gender),
+#'     pet_own = levels(feline_survey$pet_own),
+#'     y = c("no","yes")
+#'   ),
 #'   weights = feline_survey$wt,
 #'   design = formula("~.")
 #' )
+#' feline_prefs$print()
+#'
+#' head(approx_popn)
 #' popn_obj <- SurveyData$new(
-#'   approx_popn[,c("age2","gender","pet_pref")],
-#'   questions = list("Which age group are you?","Gender?","Which pet would you like to own?"),
-#'   responses = list(levels(approx_popn$age2),levels(approx_popn$gender),levels(approx_popn$pet_pref)),
+#'   data = approx_popn,
+#'   questions = list(
+#'     age2 = "Which age group are you?",
+#'     gender = "Gender?",
+#'     pet_pref = "Which pet would you like to own?"
+#'   ),
+#'   # order doesn't matter (gender before age2 here) because
+#'   # the list has the names of the variables
+#'   responses = list(
+#'     gender = levels(approx_popn$gender),
+#'     age2 = levels(approx_popn$age2),
+#'     pet_pref = levels(approx_popn$pet_pref)
+#'   ),
 #'   weights = approx_popn$wt,
 #'   design = formula("~.")
 #' )
+#' popn_obj$print()
+#'
+#' # mapping between the two surveys
 #' q1 <- SurveyQuestion$new(
 #'   name = "age",
 #'   col_names = c("age1","age2"),
@@ -46,31 +73,33 @@
 #'   values_map = data.frame("male" = "m","female" = "f", "nonbinary" = "nb")
 #' )
 #'
-#' tmp_map <- SurveyMap$new(samp_obj = feline_prefs, popn_obj = popn_obj, q1)
-#' print(tmp_map)
-#' tmp_map$validate()
-#' tmp_map$add(q3)
-#' print(tmp_map)
-#' tmp_map$delete(q3)
-#' print(tmp_map)
-#' tmp_map$add(q3)
-#' tmp_map$delete("gender")
-#' print(tmp_map)
-#' tmp_map$add(q2)
-#' print(tmp_map)
-#' tmp_map$replace(q1,q3)
-#' print(tmp_map)
-#' tmp_map$add(q1)
-#' print(tmp_map)
-#' tmp_map$validate()
-#' tmp_map$mapping()
-#' tmp_map$tabulate("age") # Just use age in the poststrat matrix
-#' tmp_map$tabulate() # Use all variables in the map
+#' # Create SurveyMap object
+#' # can add all questions at once or incrementally
+#' ex_map <- SurveyMap$new(samp_obj = feline_prefs, popn_obj = popn_obj, q1)
+#' print(ex_map)
+#' ex_map$validate()
+#' ex_map$add(q3)
+#' print(ex_map)
+#' ex_map$delete(q3)
+#' print(ex_map)
+#' ex_map$add(q3)
+#' ex_map$delete("gender")
+#' print(ex_map)
+#' ex_map$add(q2)
+#' print(ex_map)
+#' ex_map$replace(q1,q3)
+#' print(ex_map)
+#' ex_map$add(q1)
+#' print(ex_map)
+#' ex_map$validate()
+#' ex_map$mapping()
+#' ex_map$tabulate("age") # Just use age in the poststrat matrix
+#' ex_map$tabulate() # Use all variables in the map
 #'
 #'
 #' #' Example rstanarm usage
 #' #' Returns a SurveyFit object
-#' fit_1 <- tmp_map$fit(
+#' fit_1 <- ex_map$fit(
 #'   fun = rstanarm::stan_glmer,
 #'   formula = y ~ (1|age) + (1|gender),
 #'   family = "binomial",
@@ -82,7 +111,7 @@
 #' # Example brms usage
 #' # Returns a SurveyFit object
 #' # (not run because requires compilation)
-#' fit_2 <- tmp_map$fit(
+#' fit_2 <- ex_map$fit(
 #'   fun = brms::brm,
 #'   formula = y ~ (1|age) + (1|gender),
 #'   family = "bernoulli",
