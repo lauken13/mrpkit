5670c2b3bfba689de377ae01d285ed131520185c
Lauren <lauren.kennedy729@gmail.com>
2021-04-16 15:46:41 +1000

    Revert "Merge branch 'master' into WeightsIntegration"
    This reverts commit 92beea2c26358e264635a3855fabd3ca1f6b025b, reversing
    changes made to b05c780019dfefd0f33df00df95a1ed754a3f312.

--- R/SurveyData.R
+++ R/SurveyData.R
@@ -3,7 +3,7 @@
 #' @name SurveyData
 #' @export
 #' @description
-#' An [R6][R6::R6Class] `SurveyData` object represents a survey and its metadata.
+#' A `SurveyData` object represents a survey and its metadata.
 #' The survey itself is a data frame.
 #' The survey metatdata consists of the following:
 #'  - per-column questions: a list of strings
@@ -50,28 +50,32 @@
 #'
 SurveyData <- R6::R6Class(
     classname = "SurveyData",
-    private = list(
-        survey_data_ = data.frame(NULL),
-        mapped_data_ = data.frame(NULL),
-        questions_ = character(0),
-        responses_ = list(),
-        weights_ = numeric(),
-        design_ = as.formula("~.")
-    ),
     public = list(
+        survey_data = data.frame(NULL),
+        poststrat = data.frame(NULL),
+        mapped_data = data.frame(NULL),
+        questions = character(0),
+        responses = list(),
+        weights = numeric(),
+        design = list(),
         initialize = function(survey_data,
                               questions = character(0),
                               responses = list(),
                               weights = numeric(),
                               design = as.formula("~.")) {
-            if (ncol(survey_data) == 0 || nrow(survey_data) == 0) {
+            self$survey_data <- survey_data
+            self$questions <- questions
+            self$responses <- responses
+            self$weights <- weights
+            self$design <- design
+            if (ncol(survey_data) == 0) {
                 stop("survey_data cannot be empty.",
                      call. = FALSE)
             }
             # allow no question/response, else require info for all columns
-            if (length(questions) != 0 || length(responses) != 0) {
+            if (length(questions) != 0 | length(responses) != 0) {
                 if (ncol(survey_data) != length(questions) &
-                    length(questions) == sum(stats::complete.cases(questions))) {
+                    length(questions) == sum(complete.cases(questions))) {
                     stop("mismatch between number of survey_data columns and questions.",
                          call. = FALSE)
                 }
@@ -85,79 +89,18 @@ SurveyData <- R6::R6Class(
                 stop("mismatch between number of survey_data columns and weights.",
                      call. = FALSE)
             }
-            private$questions_ <- questions
-            private$responses_ <- responses
-            private$weights_ <- weights
-            private$design_ <- design
-            private$survey_data_ <- data.frame(.key = 1:nrow(survey_data), survey_data)
-            private$mapped_data_ <- data.frame(.key = 1:nrow(survey_data))
-            invisible(self)
         },
-
-        #' @description Number of observations in the survey data
-        n_obs = function() nrow(private$survey_data_),
-
-        #' @description Number of survey questions
-        n_questions = function() length(private$questions_),
-
-        #' @description Print a summary of the survey data
         print = function(...) {
-            cat("Survey with",
-                self$n_obs(), "observations,",
-                self$n_questions(), "questions",
-                "\n")
-            if (length(private$design_ == 2)) {
-                cat("Random Sampling Design \n")
-            } else {
-                cat("Simple")
-                require_suggested_package("lme4")
-                if (is.null(lme4::findbars(private$design_))){
-                    cat(" stratified sample with strata", all.vars(terms(private$design_))[[1]], "\n")
-                } else {
-                    cat(" cluster sample with cluster", all.vars(terms(private$design_))[[1]], "\n")
-                }
-            }
-            for (i in 1:ncol(self$survey_data(key = FALSE))) {
-                cat("\nColumn label:", names(self$survey_data(key = FALSE))[i], "\n")
-                if (length(private$questions_) > 0) {
-                    cat("Question:", private$questions_[i], "\n")
-                    cat("Allowed answers:",
-                        paste(private$responses_[[i]], collapse = ", "), "\n")
+            cat("Survey containing", nrow(self$survey_data), "observations", "\n")
+            for (i in 1:ncol(self$survey_data)) {
+                cat("Column ", i , " label: ", names(self$survey_data)[i], "\n")
+                if (length(self$questions) > 0) {
+                    cat("Question ", i, " (", self$questions[i], ")", "\n")
+                    cat("Allowed answers: ",
+                        paste(self$responses[[i]], collapse = ", "), "\n")
                 }
             }
             invisible(self)
-        },
-
-        add_survey_data_column = function(name, value) {
-            private$survey_data_[[name]] <- value
-            invisible(self)
-        },
-        add_mapped_data_column = function(name, value) {
-            if (ncol(private$mapped_data_)  == 0) {
-                private$mapped_data_ <- data.frame(value)
-                colnames(private$mapped_data_) <- name
-            } else {
-                private$mapped_data_[[name]] <- value
-            }
-            invisible(self)
-        },
-        survey_data = function(key = TRUE) {
-            if (key) {
-                private$survey_data_
-            } else {
-                private$survey_data_[, colnames(private$survey_data_) != ".key", drop = FALSE]
-            }
-        },
-        mapped_data = function(key = TRUE) {
-            if (key) {
-                private$mapped_data_
-            } else {
-                private$mapped_data_[, colnames(private$mapped_data_) != ".key", drop = FALSE]
-            }
-        },
-        questions = function() private$questions_,
-        responses = function() private$responses_,
-        weights = function() private$weights_,
-        design = function() private$design_
+        }
     )
 )
