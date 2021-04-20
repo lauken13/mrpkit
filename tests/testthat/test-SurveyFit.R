a832a1f94881b34f15a18e09b5f4fe899917da35
Lauren <lauren.kennedy729@gmail.com>
2021-04-20 15:15:05 +1000

    added test for error if not fitting a bernoulli/binomial model

--- tests/testthat/test-SurveyFit.R
+++ tests/testthat/test-SurveyFit.R
@@ -1,3 +1,58 @@
+# objects to use in the tests
+samp_obj <- SurveyData$new(
+  data = feline_survey,
+  questions = list(
+    age1 = "Please identify your age group",
+    gender = "Please select your gender",
+    pet_own = "Which pet do you own?",
+    y = "Response"
+  ),
+  responses = list(
+    age1 = levels(feline_survey$age1),
+    gender = levels(feline_survey$gender),
+    pet_own = levels(feline_survey$pet_own),
+    y = c("no","yes")
+  ),
+  weights = feline_survey$wt,
+  design = formula("~.")
+)
+
+popn_obj <- SurveyData$new(
+  data = approx_popn,
+  questions = list(
+    age2 = "Which age group are you?",
+    gender = "Gender?",
+    pet_pref = "Which pet would you like to own?"
+  ),
+  responses = list(
+    gender = levels(approx_popn$gender),
+    age2 = levels(approx_popn$age2),
+    pet_pref = levels(approx_popn$pet_pref)
+  ),
+  weights = approx_popn$wt,
+  design = formula("~.")
+)
+
+q1 <- SurveyQuestion$new(
+  name = "age",
+  col_names = c("age1","age2"),
+  values_map = list(
+    "18-25" = "18-35", "26-35" = "18-35","36-45" = "36-55",
+    "46-55" = "36-55", "56-65" = "56-65", "66-75" = "66+", "76-90" = "66+"
+  )
+)
+q2 <- SurveyQuestion$new(
+  name = "pet",
+  col_names = c("pet_own","pet_pref"),
+  values_map = list("cat" = "cat", "kitten" = "cat","dog" = "dog","puppy" = "dog")
+)
+q3 <- SurveyQuestion$new(
+  name = "gender",
+  col_names = c("gender","gender"),
+  values_map = data.frame("male" = "m","female" = "f", "nonbinary" = "nb")
+)
+ex_map <- SurveyMap$new(samp_obj = feline_prefs, popn_obj = popn_obj, q1)
+
 
 #Small data fits for testing purposes:
 
@@ -31,4 +86,31 @@ test_that("sim_posterior_epred throws error if not given glmerMod object", {
   expect_equal(
     sum(is.na(dim(sim_posterior_epred(glmer_fit,newdata = example_newdata)))),
     0)
+})
+
+test_that("Error if not fitting a bernoulli/binomial model", {
+  expect_error(
+    ex_map$fit(
+    fun = rstanarm::stan_glmer,
+    formula = y ~ (1|age) + (1|gender),
+    refresh = 100,
+    cores = 2
+    ),
+    "Currently only binomial and bernoulli models are supported."
+  )
+  expect_error(
+    ex_map$fit(
+      fun = lme4::glmer,
+      formula = y ~ (1|age) + (1|gender)
+    ),
+    "Currently only binomial and bernoulli models are supported."
+  )
+  expect_error(
+    ex_map$fit(
+      fun = brms::brm,
+      formula = y ~ (1|age) + (1|gender)
+    ),
+    "Currently only binomial and bernoulli models are supported."
+  )
+
 })
