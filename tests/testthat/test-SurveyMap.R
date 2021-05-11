04b9fb41105462c4975dded7e7a6a96f71995cac
jgabry <jgabry@gmail.com>
2021-05-10 19:57:38 -0600

    clean up tests-SurveyMap.R and add notes about failing tests

--- tests/testthat/test-SurveyMap.R
+++ tests/testthat/test-SurveyMap.R
@@ -341,62 +341,9 @@ test_that("validate creates correct levels (example4)", {
                   c("A + B","C","D + E"))
 })
 
-
-samp_obj <- SurveyData$new(
-  data = feline_survey,
-  questions = list(
-    age1 = "Please identify your age group",
-    gender = "Please select your gender",
-    pet_own = "Which pet do you own?",
-    y = "Response"
-  ),
-  responses = list(
-    age1 = levels(feline_survey$age1),
-    gender = levels(feline_survey$gender),
-    pet_own = levels(feline_survey$pet_own),
-    y = c("no","yes")
-  ),
-  weights = feline_survey$wt,
-  design = formula("~.")
-)
-
-popn_obj <- SurveyData$new(
-  data = approx_popn,
-  questions = list(
-    age2 = "Which age group are you?",
-    gender = "Gender?",
-    pet_pref = "Which pet would you like to own?"
-  ),
-  responses = list(
-    gender = levels(approx_popn$gender),
-    age2 = levels(approx_popn$age2),
-    pet_pref = levels(approx_popn$pet_pref)
-  ),
-  weights = approx_popn$wt,
-  design = formula("~.")
-)
-
-q1 <- SurveyQuestion$new(
-  name = "age",
-  col_names = c("age1","age2"),
-  values_map = list(
-    "18-25" = "18-35", "26-35" = "18-35","36-45" = "36-55",
-    "46-55" = "36-55", "56-65" = "56-65", "66-75" = "66+", "76-90" = "66+"
-  )
-)
-q2 <- SurveyQuestion$new(
-  name = "pet",
-  col_names = c("pet_own","pet_pref"),
-  values_map = list("cat" = "cat", "kitten" = "cat","dog" = "dog","puppy" = "dog")
-)
-q3 <- SurveyQuestion$new(
-  name = "gender",
-  col_names = c("gender","gender"),
-  values_map = data.frame("male" = "m","female" = "f", "nonbinary" = "nb")
-)
-
-ex_map <- SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
-
+# This test is currently failing. Should it be?
+# Expected match: "Predictor variables not known in population."
+# Actual message: "Not all variables available in the data. Missing vars:  gender"
 test_that("Error if predictor vars not included in poststrat matrix",{
   ex_map1 <-  SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2)
   ex_map1$mapping()
@@ -435,8 +382,6 @@ test_that("Error if predictor vars not included in poststrat matrix",{
     "Predictor variables not known in population.", fixed = TRUE
   )
 })
-
-
 
 test_that("Error if vars not included in data",{
   ex_map1 <-  SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1,q2,q3)
@@ -499,21 +444,19 @@ test_that("Error if vars not included in data",{
   )
 })
 
-
 test_that("Error if not fitting a bernoulli/binomial model", {
+  ex_map1 <- SurveyMap$new(samp_obj, popn_obj, q1, q2, q3)
   skip_if_not_installed("rstanarm")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = rstanarm::stan_glmer,
-      formula = y ~ (1|age) + (1|gender),
-      refresh = 100,
-      cores = 2
+      formula = y ~ (1|age) + (1|gender)
     ),
     "Currently only binomial and bernoulli models are supported."
   )
   skip_if_not_installed("lme4")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = lme4::glmer,
       formula = y ~ (1|age) + (1|gender)
     ),
@@ -521,7 +464,7 @@ test_that("Error if not fitting a bernoulli/binomial model", {
   )
   skip_if_not_installed("brms")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = brms::brm,
       formula = y ~ (1|age) + (1|gender)
     ),
@@ -530,64 +473,13 @@ test_that("Error if not fitting a bernoulli/binomial model", {
 
 })
 
-
+# this test will fail until https://github.com/mitzimorris/mrp-kit/issues/59
+# because the mapped data has already be
 test_that("Error if data hasn't been mapped yet",{
-  samp_obj <- SurveyData$new(
-    data = feline_survey,
-    questions = list(
-      age1 = "Please identify your age group",
-      gender = "Please select your gender",
-      pet_own = "Which pet do you own?",
-      y = "Response"
-    ),
-    responses = list(
-      age1 = levels(feline_survey$age1),
-      gender = levels(feline_survey$gender),
-      pet_own = levels(feline_survey$pet_own),
-      y = c("no","yes")
-    ),
-    weights = feline_survey$wt,
-    design = formula("~.")
-  )
-
-  popn_obj <- SurveyData$new(
-    data = approx_popn,
-    questions = list(
-      age2 = "Which age group are you?",
-      gender = "Gender?",
-      pet_pref = "Which pet would you like to own?"
-    ),
-    responses = list(
-      gender = levels(approx_popn$gender),
-      age2 = levels(approx_popn$age2),
-      pet_pref = levels(approx_popn$pet_pref)
-    ),
-    weights = approx_popn$wt,
-    design = formula("~.")
-  )
-
-  q1 <- SurveyQuestion$new(
-    name = "age",
-    col_names = c("age1","age2"),
-    values_map = list(
-      "18-25" = "18-35", "26-35" = "18-35","36-45" = "36-55",
-      "46-55" = "36-55", "56-65" = "56-65", "66-75" = "66+", "76-90" = "66+"
-    )
-  )
-  q2 <- SurveyQuestion$new(
-    name = "pet",
-    col_names = c("pet_own","pet_pref"),
-    values_map = list("cat" = "cat", "kitten" = "cat","dog" = "dog","puppy" = "dog")
-  )
-  q3 <- SurveyQuestion$new(
-    name = "gender",
-    col_names = c("gender","gender"),
-    values_map = data.frame("male" = "m","female" = "f", "nonbinary" = "nb")
-  )
-  ex_map <-  SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
+  ex_map1 <-  SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
   skip_if_not_installed("lme4")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = lme4::glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -596,7 +488,7 @@ test_that("Error if data hasn't been mapped yet",{
   )
   skip_if_not_installed("rstanarm")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = rstanarm::stan_glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -605,7 +497,7 @@ test_that("Error if data hasn't been mapped yet",{
   )
   skip_if_not_installed("brms")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = brms::brm,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -613,14 +505,12 @@ test_that("Error if data hasn't been mapped yet",{
     "Mapped data not found.", fixed = TRUE
   )
 })
-
-#Create the mapped data
-ex_map$mapping()
 
 test_that("Error if poststrat matrix hasn't been created yet",{
+  ex_map1 <- SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
   skip_if_not_installed("lme4")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = lme4::glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -629,7 +519,7 @@ test_that("Error if poststrat matrix hasn't been created yet",{
   )
   skip_if_not_installed("rstanarm")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = rstanarm::stan_glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -638,7 +528,7 @@ test_that("Error if poststrat matrix hasn't been created yet",{
   )
   skip_if_not_installed("brms")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = brms::brm,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit")
@@ -648,9 +538,10 @@ test_that("Error if poststrat matrix hasn't been created yet",{
 })
 
 test_that("Error if data is given as input",{
+  ex_map1 <- SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
   skip_if_not_installed("lme4")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = lme4::glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit"),
@@ -660,7 +551,7 @@ test_that("Error if data is given as input",{
   )
   skip_if_not_installed("rstanarm")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = rstanarm::stan_glmer,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit"),
@@ -670,7 +561,7 @@ test_that("Error if data is given as input",{
   )
   skip_if_not_installed("brms")
   expect_error(
-    ex_map$fit(
+    ex_map1$fit(
       fun = brms::brm,
       formula = y ~ (1|age) + (1|gender),
       family = binomial(link="logit"),
@@ -680,12 +571,12 @@ test_that("Error if data is given as input",{
   )
 })
 
-ex_map$tabulate() # Use all variables in the map
-
-
-test_that("Warning is given if fitting using packages that are not lme4, brms, rstanarm ",{
+test_that("Warning is given if fitting using packages that are not lme4, brms, rstanarm ", {
+  ex_map1 <- SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
+  ex_map1$mapping()
+  ex_map1$tabulate()
   expect_warning(
-    ex_map$fit(
+    ex_map1$fit(
       fun = stats::glm,
       formula = y ~ age + gender,
       family = "binomial"
@@ -696,6 +587,9 @@ test_that("Warning is given if fitting using packages that are not lme4, brms, r
 
 
 test_that("Model fits do not cause errors if specified correctly",{
+  ex_map1 <- SurveyMap$new(samp_obj = samp_obj, popn_obj = popn_obj, q1, q2, q3)
+  ex_map1$mapping()
+  ex_map1$tabulate()
   skip_if_not_installed("rstanarm")
   expect_error(suppressWarnings(ex_map$fit(
     fun = rstanarm::stan_glmer,
