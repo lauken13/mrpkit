6f32fbc6454ed437aad9b4388b1d1b6509a9ebdc
Lauren <lauren.kennedy729@gmail.com>
2021-06-22 11:38:05 +1000

    Added doc

--- R/SurveyMap.R
+++ R/SurveyMap.R
@@ -344,7 +344,13 @@ SurveyMap <- R6::R6Class(
       invisible(self)
     },
 
-    #' @description Prepare the mapped data
+    #' @description The mapping method uses the given maps between questions to create new sample and population data
+    #' that has unified variable names (i.e., if the underlying construct is called `age`, both sample and population will have
+    #' an `age` column, even if in the the raw data both had different variable names). The method also unifies the levels of each
+    #' variable in the sample and population so that the maximum set of consistent levels is created. Names of these new levels are
+    #' made according the the sample level names. If multiple levels are combined, the new name will be existing levels seperated by a
+    #' ` + `.
+    #'
     mapping  = function() {
       for (j in 1:length(private$item_map_)) {
         samp_mapnames <- private$item_map_[[j]]$col_names()[1]
@@ -357,7 +363,10 @@ SurveyMap <- R6::R6Class(
         new_levels_samp <- character(length(levels_map_samp))
         new_levels_popn <- character(length(levels_map_popn))
 
-        # Find the naming clusters of levels  (loosely inspired by complete linkage clustering algorithms)
+        # Find the naming clusters of levels  (loosely inspired by complete linkage clustering algorithms.
+        # Three major steps:
+        # Step 1:  Create a matrix that has the factor level labels for the sample as row names, and factor level labels for the
+        # population as the column names. If the levels are linked, record 1 in the corresponding entry, otherwise record 0.
         mapped_levels <- matrix(0,nrow=length(unique(levels_map_samp)),ncol = length(unique(levels_map_popn)))
         colnames(mapped_levels)<-levels_popn
         row.names(mapped_levels)<-levels_samp
@@ -373,6 +382,9 @@ SurveyMap <- R6::R6Class(
         if(sum(colSums(mapped_levels, na.rm=TRUE)!=0)<ncol(mapped_levels)){
           stop("Levels: ",paste(colnames(sum(colSums(mapped_levels, na.rm=TRUE)!=0)),collapse = " ")," do not have a match in the sample.")
         }
+
+        # Moving down the rows (i.e., for each factor level in the sample), identify if there are multiple corresponding population
+        # values. If there are, merge the columns for the values together and combine the col names together, seperated by a ' + ' symbol.
         tmp_mapped_levels <- mapped_levels
         mapped_levels_new <- mapped_levels
         for (unique_samp_levels in 1:length(mapped_levels[,1])) {
@@ -383,6 +395,10 @@ SurveyMap <- R6::R6Class(
             colnames(mapped_levels_new)[ll+1] <- paste0(colnames(tmp_mapped_levels[,tmp_mapped_levels[unique_samp_levels,,drop = FALSE]==1,drop = FALSE]), collapse = " + ")
             tmp_mapped_levels<- mapped_levels_new        }
         }
+
+        # Moving across the now abridged columns (i.e., for every new facter level in the popn), identify if there are multiple
+        # corresponding sample values. If there are, merge the rows for the values together and combine the row names (sample factor levels)
+        # together, seperated by a " + " symbol
 
         tmp_mapped_levels <- mapped_levels_new
         mapped_levels_fin <- mapped_levels_new
@@ -397,7 +413,8 @@ SurveyMap <- R6::R6Class(
         }
         mapped_levels_fin <- mapped_levels_new
 
-        # Create the name remapping
+        # Create the name remapping, with the old names as the previous factor levels, and the names of the vector as the new levels
+        # For the sample
         new_levels_samp_names <- length(new_levels_samp)
         for(samp_level in 1:length(levels_samp)){
           if(length(grep(levels_samp[samp_level],row.names(mapped_levels_fin)))>1){
@@ -410,7 +427,7 @@ SurveyMap <- R6::R6Class(
         }
         new_levels_samp <- levels_samp
         names(new_levels_samp) <- new_levels_samp_names
-
+        # For the population
         new_levels_popn_names <- length(new_levels_popn)
         for(popn_level in 1:length(levels_popn)){
           if(length(grep(paste0(levels_popn[popn_level]),colnames(mapped_levels_fin)))>1){
@@ -418,8 +435,8 @@ SurveyMap <- R6::R6Class(
           } else{
             popn_level_loc <- grep(levels_popn[popn_level],colnames(mapped_levels_fin))
           }
-          #name according to sample data
-          new_levels_popn_names[popn_level] <- row.names(mapped_levels_fin)[popn_level_loc]
+          #name the population levels according to the sample data
+          new_levels_popn_names[popn_level] <- row.names(mapped_levels_fin)[mapped_levels_fin[,popn_level_loc]==1]
         }
         new_levels_popn <- levels_popn
         names(new_levels_popn) <- new_levels_popn_names
