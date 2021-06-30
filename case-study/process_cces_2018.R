582bd42c25a8643b47dbf1c7e52f265a4ceda5f4
Mitzi Morris <mitzi@panix.com>
2021-06-30 11:57:38 -0400

    checkpointing case study

--- case-study/process_cces_2018.R
+++ case-study/process_cces_2018.R
@@ -1,9 +1,3 @@
-setwd("~/github/cces/dataverse")
-
-# read 2018 data
-#cces_2018 <- get(load("cces18_common_vv.RData"))
-#dim(cces_2018)
-
 library(readstata13)
 cces_2018 = read.dta13("cces18_common_vv.dta", convert.factors = T, nonint.factors = T, generate.factors=T)
 
@@ -17,9 +11,9 @@ demo_cols <- c(1, which(col_names == "birthyr"),
               which(col_names == "race"),
               which(col_names == "hispanic"),
               which(col_names == "marstat"),
-              which(col_names == "region"),
-              which(col_names == "employ"),
-              which(col_names == "hadjob"),
+#              which(col_names == "region"),
+#              which(col_names == "employ"),
+#              which(col_names == "hadjob"),
               which(col_names == "inputstate"))
 col_labels[demo_cols]
 
@@ -40,8 +34,6 @@ table(cces_2018_subset$gender)
 table(cces_2018_subset$educ)
 table(cces_2018_subset$race)
 table(cces_2018_subset$marstat)
-table(cces_2018_subset$employ)
-table(cces_2018_subset$region)
 length(table(cces_2018_subset$inputstate))
 length(table(cces_2018_subset$birthyr))
 
@@ -49,9 +41,29 @@ length(table(cces_2018_subset$birthyr))
 (length(table(cces_2018_subset$educ))-2) *
 (length(table(cces_2018_subset$race))-2) *
 (length(table(cces_2018_subset$marstat))-2) *
-(length(table(cces_2018_subset$employ))-2) *
-(length(table(cces_2018_subset$region))-2) *
 length(table(cces_2018_subset$inputstate)) *
 length(table(cces_2018_subset$birthyr))
 
 save(cces_2018_subset, file="cces_2018_subset.RData")
+
+## # viz demographics
+## demographics_2018 = cces_2018[demo_cols]
+## demo_names = names(demographics_2018)
+## library(tidyverse)
+## categories_bins_plot <- function(df, column_name)
+## {
+##     df2 = as.data.frame(group_by_(df, column_name) %>% summarise(n = n()))
+##     ggplot(df2, aes(x=df2[,1], y=df2[,2])) +
+##         geom_bar(stat = "identity") +
+##         xlab(column_name) + ylab(element_blank()) +
+##         ggtitle(paste("CCES 2018", column_name, "raw counts")) +
+##         theme(axis.text.x = element_text(angle = 90, hjust = 1))
+## }
+
+## for(i in 1:length(demo_names)) {
+##     name = paste("cces_2018_",demo_names[i],".png", sep="", collapse="")
+##     p = categories_bins_plot(demographics_2018, demo_names[i])
+##     png(filename=name)
+##     show(p)
+##     dev.off()
+## }
