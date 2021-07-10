5f69022d16e675fb79821e685c94859781b659f6
Mitzi Morris <mitzi@panix.com>
2021-07-10 01:07:18 -0400

    checkpointing; created SurveyData objects

--- case-study/process_cces_2018.R
+++ case-study/process_cces_2018.R
@@ -1,69 +1,48 @@
 library(readstata13)
-cces_2018 = read.dta13("cces18_common_vv.dta", convert.factors = T, nonint.factors = T, generate.factors=T)
 
-attr_names <- names(attributes(cces_2018))
+cces_2018 = read.dta13("cces18_common_vv.dta", convert.factors = T, nonint.factors = T, generate.factors=T)
 col_labels <- attr(cces_2018, "var.labels")
 col_names <- attr(cces_2018, "names")
 
-demo_cols <- c(1, which(col_names == "birthyr"),
+demo_cols <- c(which(col_names == "birthyr"),
               which(col_names == "gender" ),
               which(col_names == "educ"),
               which(col_names == "race"),
               which(col_names == "hispanic"),
               which(col_names == "marstat"),
-#              which(col_names == "region"),
-#              which(col_names == "employ"),
-#              which(col_names == "hadjob"),
               which(col_names == "inputstate"))
-col_labels[demo_cols]
+keep_rows <- complete.cases(cces_2018[, demo_cols]) # no missing demographics
 
 outcome_cols <- c(which(startsWith(col_labels, "Abortion --")),
                  which(startsWith(col_labels, "Immigration --")),
                  which(startsWith(col_labels, "Taxes --")),
                  which(startsWith(col_labels, "Health Care --")),
                  which(startsWith(col_labels, "President 20")))
-col_labels[outcome_cols]
-
 keep_cols <- sort(c(demo_cols, outcome_cols))
 
-cces_2018_subset <- subset(cces_2018[keep_cols])
-attr(cces_2018_subset, "var.labels") <- col_labels[keep_cols]
-attr(cces_2018_subset, "names") <- col_names[keep_cols]
+cces_2018_subset <- subset(cces_2018, keep_rows, keep_cols)
+# `subset` drops attributes, add back the ones we need
+attr(cces_2018_subset, "var.labels") <- attr(cces_2018, "var.labels")[keep_cols]
+droplevels(cces_2018_subset)
 
-table(cces_2018_subset$gender)
-table(cces_2018_subset$educ)
-table(cces_2018_subset$race)
-table(cces_2018_subset$marstat)
-length(table(cces_2018_subset$inputstate))
-length(table(cces_2018_subset$birthyr))
+cces_race2 <- cces_2018_subset$race
+cces_race2[cces_2018_subset$hispan == "Yes"] <- "Hispanic"
+# add to acs_2018_subset data.frame - respect column types
+cces_2018_subset$race2 = cces_race2
+attr(cces_2018_subset, "var.labels")[['race2']] <- "Race or Hispanic"
 
-(length(table(cces_2018_subset$gender))-2) *
-(length(table(cces_2018_subset$educ))-2) *
-(length(table(cces_2018_subset$race))-2) *
-(length(table(cces_2018_subset$marstat))-2) *
-length(table(cces_2018_subset$inputstate)) *
-length(table(cces_2018_subset$birthyr))
+
+# bin CCES birthyear
+cohort_breaks = c(1900, 1935, 1945, 1955, 1965, 1975, 1985, 2000)
+cohort_labels=c("before-1936",
+         "1936-1945",
+         "1946-1955",
+         "1956-1965",
+         "1966-1975",
+         "1976-1985",
+         "after 1986")
+cces_2018_subset$birthyr_cohorts = cut(cces_2018_subset$birthyr, cohort_breaks, cohort_labels)
+attr(cces_2018_subset, "var.labels")[['birthyr_cohort']] <- "Birthyear cohort"
+
 
 save(cces_2018_subset, file="cces_2018_subset.RData")
-
-## # viz demographics
-## demographics_2018 = cces_2018[demo_cols]
-## demo_names = names(demographics_2018)
-## library(tidyverse)
-## categories_bins_plot <- function(df, column_name)
-## {
-##     df2 = as.data.frame(group_by_(df, column_name) %>% summarise(n = n()))
-##     ggplot(df2, aes(x=df2[,1], y=df2[,2])) +
-##         geom_bar(stat = "identity") +
-##         xlab(column_name) + ylab(element_blank()) +
-##         ggtitle(paste("CCES 2018", column_name, "raw counts")) +
-##         theme(axis.text.x = element_text(angle = 90, hjust = 1))
-## }
-
-## for(i in 1:length(demo_names)) {
-##     name = paste("cces_2018_",demo_names[i],".png", sep="", collapse="")
-##     p = categories_bins_plot(demographics_2018, demo_names[i])
-##     png(filename=name)
-##     show(p)
-##     dev.off()
-## }
--- case-study/process_ipums_acs_2018.R
+++ case-study/process_ipums_acs_2018.R
@@ -1,20 +1,50 @@
-# NOTE: To load data, you must download both the extract's data and the DDI
-# and also set the working directory to the folder with these files (or change the path below).
+if (!require("ipumsr")) stop("This case study requires ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
+if (!require("labelled")) stop("This case study requires labelled package. It can be installed using the following command: install.packages('labelled')")
+library(ipumsr)
+library(labelled)
+library(readstata13)
 
-if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
 
 # download dataset from IPUMS
-# xml and dat file should have same name, be in same directory
+# xml ddi file and Stata dat file should have same name, be in same directory
 acs_2018_ddi <- read_ipums_ddi("usa_00007.xml")
 acs_2018 <- read_ipums_micro(acs_2018_ddi)
 
 # save object for later
-save(acs_2018, acs_2018_ddi, file= "acs_2018_subset.RData")
+save(acs_2018, acs_2018_ddi, file= "acs_2018_subset_raw.RData")
 
-acs_colnames <- names(acs_2018)
-print(acs_colnames)
-print(class(acs_colnames[1]))
-for (i in 1:length(acs_colnames)) {
-    print(acs_colnames[i])
-    print(ipums_val_labels(acs_2018[[i]]))
-}
+# drop underage, non-citizens
+citizen = as_factor(acs_2018$CITIZEN) != "Not a citizen"
+over_18 = acs_2018$AGE > 17
+acs_2018_subset = acs_2018[(over_18 & citizen) , ]  
+
+# create column which blends "race" "hispan"
+acs_race2 <- factor(as_factor(acs_2018_subset$RACE), levels=c(levels(as_factor(acs_2018_subset$RACE)), "Hispanic"))
+
+# if "hispan" is "Yes", "race" label is "Hispanic".
+is_hispan <- acs_2018_subset$HISPAN > 0 & acs_2018_subset$HISPAN < 5
+acs_race2[is_hispan==TRUE] <- "Hispanic"
+acs_2018_subset$race2 = to_labelled(acs_race2)
+
+# create column which conflates spouse present, absent
+acs_marst2 <- as_factor(acs_2018_subset$MARST)
+levels(acs_marst2) = c(levels(acs_marst2), "Married")
+acs_marst2[acs_marst2 =="Married, spouse present"] <- "Married"
+acs_marst2[acs_marst2 =="Married, spouse absent"] <- "Married"
+
+# add to acs_2018_subset data.frame - respect column types
+acs_2018_subset$marst2 = to_labelled(droplevels(acs_marst2))
+
+acs_birthyr = 2018 - acs_2018_subset$AGE
+cohort_breaks = c(1900, 1935, 1945, 1955, 1965, 1975, 1985, 2000)
+cohort_labels=c("before-1936",
+         "1936-1945",
+         "1946-1955",
+         "1956-1965",
+         "1966-1975",
+         "1976-1985",
+         "after 1986")
+birthyr_cohorts = cut(acs_birthyr, cohort_breaks, cohort_labels)
+acs_2018_subset$birthyr_cohorts = to_labelled(birthyr_cohorts)
+
+save(acs_2018_subset, acs_2018_ddi, file="acs_2018_subset.RData")
