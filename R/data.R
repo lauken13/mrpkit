72288b140d4c5dba1d773609486729be69c33562
Dewi-Amaliah <dama0007@student.monash.edu>
2021-08-30 22:19:44 +1000

    Add data documentation for shape_survey_haven.rda.

--- R/data.R
+++ R/data.R
@@ -57,6 +57,29 @@
 #'
 "shape_survey"
 
+#' Simulated Survey Data for Shape World - Haven-labelled Version
+#'
+#' This is the shape_survey data with some variables saved in haven-labelled class.
+#'
+#' @source Created in ShapeWorldVoters.R
+#'
+#' @format A data frame with columns:
+#'
+#' \describe{
+#'  \item{age}{Age group; haven-labelled}
+#'  \item{gender}{Gender; haven-labelled}
+#'  \item{vote_for}{Party voted for in the 2016 election; haven-labelled}
+#'  \item{highest_educ}{Highest grade of education completed; haven-labelled}
+#'  \item{state}{State; haven-labelled}
+#'  \item{y}{Preference for BP over CP in 2020 election}
+#'  \item{wt}{Raked and trimmed survey weight}
+#' }
+#' @examples
+#'
+#'  shape_survey_haven
+#'
+"shape_survey_haven"
+
 #' Simulated approximate population data for the Shape World survey
 #'
 #' Simulated approximate population data for the [Shape World][shape_survey]
