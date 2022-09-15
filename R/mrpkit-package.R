f205788536fa63e502103140980d46eaace28c20
jgabry <jgabry@gmail.com>
2022-09-15 13:15:41 -0600

    add `@importFrom` statements to avoid CRAN NOTE during check

--- R/mrpkit-package.R
+++ R/mrpkit-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 #' @importFrom dplyr %>%
+#' @importFrom R6 R6Class
+#' @importFrom ggplot2 ggplot
+#' @importFrom tidyr pivot_longer
+#'
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
