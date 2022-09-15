c9f5f19ef0889e93b7e60a649e184837a174efd3
jgabry <jgabry@gmail.com>
2022-09-15 13:19:08 -0600

    update NAMESPACE

--- R/mrpkit-package.R
+++ R/mrpkit-package.R
@@ -5,6 +5,7 @@
 #' @importFrom R6 R6Class
 #' @importFrom ggplot2 ggplot
 #' @importFrom tidyr pivot_longer
+#' @importFrom forcats fct_recode
 #'
 
 # The following block is used by usethis to automatically manage
